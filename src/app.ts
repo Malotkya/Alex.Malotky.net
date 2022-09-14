fd3409c4257facb014df5ea93a731e206bf39e0c
Alex Malotky <ajmalotky@madisoncollege.edu>
2022-09-14 15:52:33 -0500

    changes to public folder(not set in stone yet)

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import logger from 'morgan';
 import helmet from 'helmet';
 
-let app: Application = express();
+export const app: Application = express();
 
 app.use(helmet());
 app.use(helmet.contentSecurityPolicy({
@@ -17,19 +17,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 if (process.env.NODE_ENV == "debug") {
-    app.use(express.static(path.join(process.cwd(), 'public')));
+    app.use(express.static(path.join(process.cwd(), '../public')));
 }
-
 
 //Routes
 import { Index, FourZeroFour, ErrorHandler } from "./index/router";
 import { Admin } from "./admin/router";
+import { Resume } from "./resume/router";
 
 app.use('/', Index);
 app.use('/Admin', Admin);
+app.use('/Resume', Resume);
 
-// catch 404 and forward to error handler
+// catch 404 and other errors
 app.use(FourZeroFour);
 app.use(ErrorHandler);
-
-module.exports = app;

31c9a7d6a10f9b0d506fab4eb6217f2bb0976893
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-05-08 16:45:28 -0500

    Created Resume Router

---
+++ src/Resume.ts
@@ -0,0 +1,43 @@
+import Router from "./App/Router";
+import {render, execute} from "./App";
+
+/** Resume Router
+ * 
+ * @author Alex Malotky
+ */
+export const Resume = new Router("/Resume", "Resume", "Alex's resume and other skills.");
+
+Resume.onLoad(()=>{
+
+    // Imitating database results to show template rendering functionality.
+    const schooling = [
+        {
+            schoolName: "Madison Area Technical College",
+            graduated: "Dec, 2019",
+            degree: "Associates in Applied Science - Web Development"
+        },
+        {
+            schoolName: "University of Wisconsin - Platteville",
+            graduated: "Dec, 2023",
+            degree: "Basholors of Science in Applied Computing"
+        }
+    ];
+    const work = [
+        {
+            employer: "Walmart",
+            title: "Sales Associate",
+            startDate: "Sept, 2016",
+            endDate: "Aug, 2018"
+        },
+        {
+            employer: "Permar Security Services",
+            title: "Security Officer",
+            startDate: "May, 2018"
+        }
+    ];
+
+    return render("resume.html", {
+        schools: schooling,
+        jobs: work
+    });
+});

e123f1aa3f30c3b39bbaf4796b57370097469074
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-05-09 13:08:02 -0500

    Moved files for new directory structure

---
+++ src/static/scripts/home.js
@@ -0,0 +1,3 @@
+console.log("Welcome to AlexMalotky.com");
+console.log("I am printing to the console from code that is loaded dynamically, after the page has been loaded.");
+console.log("You can see how I do this with the execute function at:\n https://github.com/Malotkya/AlexMalotky.com/blob/main/src/App/index.ts");

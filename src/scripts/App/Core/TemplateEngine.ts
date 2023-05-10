0aa320273f983ffc1ed1863849680963faaf32b7
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-05-10 07:37:16 -0500

    combined passed values to single objects

--- src/scripts/App/Core/TemplateEngine.ts
+++ src/scripts/App/Core/TemplateEngine.ts
@@ -74,23 +74,22 @@ export function compile(buffer: Array<string>, args?: any): string{
         
     const string = buffer.join("");
 
-    const objectNames =  Object.keys(args);
-    const objectValues = Object.values(args);
+    let names =  Object.keys(args);
+    let values = Object.values(args);
 
-    const functionNames = Object.keys(FUNCTIONS);
-    const functionValues = Object.values(FUNCTIONS);
+    names = names.concat(Object.keys(FUNCTIONS));
+    values = values.concat(Object.values(FUNCTIONS));
 
     while (true){
         try {
-            return new Function(...functionNames, ...objectNames,
-                `return \`${string}\`;`)(...functionValues, ...objectValues);
+            return new Function(...names, `return \`${string}\`;`)(...values);
 
         } catch(error){
             if(typeof error.message === "string"){
                 const index = error.message.indexOf(" is not defined");
                 if(index > 0){
-                    objectNames.push(error.message.substring(0, index));
-                    objectValues.push("undefined");
+                    names.push(error.message.substring(0, index));
+                    values.push("undefined");
                 } else {
                     throw error;
                 }

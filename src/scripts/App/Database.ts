746fbcaaf6ffe74b8126af1edf4106243c02b520
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-05-15 12:31:01 -0500

    Added Firestore to App

---
+++ src/scripts/App/Database.ts
@@ -0,0 +1,58 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, Firestore, collection, getDocs } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyBLLSqjAFfA5EFgQ0Y_L5Ed24vhnpl_dkE",
+  authDomain: "alexmalotky-com.firebaseapp.com",
+  databaseURL: "https://alexmalotky-com.firebaseio.com",
+  projectId: "alexmalotky-com",
+  storageBucket: "alexmalotky-com.appspot.com",
+  messagingSenderId: "76606919461",
+  appId: "1:76606919461:web:2a2b1fd5e67da5bc22c0cc",
+  measurementId: "G-GT065KPVZ7"
+};
+
+export default class Database{
+    private _firestore: Firestore;
+    constructor(){
+        const app = initializeApp(firebaseConfig);
+        this._firestore = getFirestore(app);
+    }
+
+    async resume(){
+        const raw = await getDocs(collection(this._firestore, "Resume"));
+
+        const schools = [];
+        const jobs = [];
+        const skills = [];
+        const unknown = [];
+
+        raw.forEach(result=>{
+            switch(result.data().type){
+                case "school":
+                schools.push(result.data());
+                break;
+
+                case "job":
+                jobs.push(result.data());
+                break;
+
+                case "skill":
+                skills.push(result.data());
+                break;
+
+                default:
+                unknown.push(result.data());
+            }
+        });
+
+        return {
+            schools: schools,
+            jobs: jobs,
+            skills: skills,
+            unknown: unknown
+        };
+    }
+}

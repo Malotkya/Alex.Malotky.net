6275f20324b29b908182c47e534bd8d0fd2c9c81
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-05-15 14:53:17 -0500

    formatDate now returns date if it is a string

--- src/scripts/App/Core/TemplateEngine.ts
+++ src/scripts/App/Core/TemplateEngine.ts
@@ -160,71 +160,62 @@ function forEach(arr:Array<any>, loopCallback:(value:any, index:string)=>string,
     return buffer;
 }
 
+// Constants used by the formatDate function.
 const LONG_MONTH = [
-    "January",
-    "Febuary",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
+    "January", "Febuary", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+]; const SHORT_MONTH = [
+    "Jan", "Feb", "Mar", "Apr", "May", "June",
+    "July", "Aug", "Sept", "Oct", "Nov", "Dec"
+]; const LONG_DAY = [
+    "Sunday", "Monday", "Tuesday", "Wednesday",
+    "Tursday", "Friday", "Saturday"
+]; const SHORT_DAY = [
+    "Sun", "Mon", "Tue", "Wend",
+    "Thur", "Fri", "Sat"
 ];
 
-const SHORT_MONTH = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "June",
-    "July",
-    "Aug",
-    "Sept",
-    "Oct",
-    "Nov",
-    "Dec"
-]
-
-const LONG_DAY = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Tursday",
-    "Friday",
-    "Saturday"
-]
-
-const SHORT_DAY = [
-    "Sun",
-    "Mon",
-    "Tue",
-    "Wend",
-    "Thur",
-    "Fri",
-    "Sat"
-]
-
+//Interface for firebaseDate format.
 interface firebaseDate{
-    toDate: ()=>Date
+    seconds: number,
+    nanoseconds: number
 }
 
-function formatDate(date: firebaseDate|Date, format: string){
-    if(!(date instanceof Date))
-        date = date.toDate();
-
+/** Format Date Function
+ * 
+ * @param {Date|firebaseDate|string} date 
+ * @param {string} format: %Y   - year as 4 char string
+ *                         %y   - year as 2 char string
+ *                         %M-d - month as a number
+ *                         %M   - month as a string
+ *                         %m   - month as abreviated string
+ *                         %W   - day of week as string
+ *                         %w   - day of week as abreviated string
+ *                         $H   - hour as number 0-23
+ *                         %h   - hour as number 1-12
+ *                         %N   - minute as number
+ *                         $S   - second as number
+ *                         $s   - milisecond as number
+ * 
+ * @returns {string}
+ */
+function formatDate(date: Date|firebaseDate|string, format: string): string{
+    if(!(date instanceof Date)) {
+        if(typeof date === "string")
+            //For some reasone sometimes the date format is remembered rather then being refreshed,
+            //we will just return it.
+            return date;
+        else
+            date = new Date(date.seconds*1000 + date.nanoseconds/100000);
+    }
+        
     return format
         //Year
         .replace("%Y", date.getFullYear().toString())
         .replace("%y", date.getFullYear().toString().substring(2))
 
         //Month
-        .replace("%M-d", date.getMonth().toString())
+        .replace("%M-n", date.getMonth().toString())
         .replace("%M", LONG_MONTH[date.getMonth()])
         .replace("%m", SHORT_MONTH[date.getMonth()])
 

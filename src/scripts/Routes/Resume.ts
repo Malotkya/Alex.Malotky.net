2b90721ed7261f4dacb76ff2b97ae6eb7d4e1cd8
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-05-15 15:01:42 -0500

    Removed simulated results, no using firestore.

--- src/scripts/Routes/Resume.ts
+++ src/scripts/Routes/Resume.ts
@@ -11,39 +11,8 @@ const database = new Database();
 
 Resume.onLoad(async()=>{
 
-    let results = await database.resume();
-    if(results.unknown.length > 0)
-        console.warn("Unknown results found:\n" + JSON.stringify(results.unknown, null, 2));
-    
-    // Imitating database results to show template rendering functionality.
-    const schooling = [
-        {
-            schoolName: "Madison Area Technical College",
-            graduated: "Dec, 2019",
-            degree: "Associates in Applied Science - Web Development"
-        },
-        {
-            schoolName: "University of Wisconsin - Platteville",
-            graduated: "Dec, 2023",
-            degree: "Basholors of Science in Applied Computing"
-        }
-    ];
-    const work = [
-        {
-            employer: "Walmart",
-            title: "Sales Associate",
-            startDate: "Sept, 2016",
-            endDate: "Aug, 2018"
-        },
-        {
-            employer: "Permar Security Services",
-            title: "Security Officer",
-            startDate: "May, 2018"
-        }
-    ];
-    const skills = [
-        /* Coming Soon */
-    ]
+    let results = await database.resume();  
+    console.log(results);
 
     return render("resume.html", results);
 });

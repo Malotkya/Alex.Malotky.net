3b7244074b271a5696bf0a1d266529373af0af03
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-08-17 14:44:26 -0500

    Moved complicated render function to Router class.

--- src/scripts/App/Core/Content.ts
+++ src/scripts/App/Core/Content.ts
@@ -1,11 +1,3 @@
-import { sleep } from "..";
-import {makeErrorMessage} from "./App_Base";
-
-export interface execute{
-    args: any,
-    function: (args:any)=>Promise<void>|void
-}
-
 /** Content Class
  * 
  * Base Class for Routers
@@ -44,28 +36,6 @@ export default class Countent{
         this._string = html;
     }
 
-    /** Get HTML string.
-     * 
-     */
-    get html():Promise<string>{
-        return new Promise( (resolve, reject)=>{
-            if(typeof this._string === "undefined")
-                reject(new Error("No html given"));
-
-            resolve(this._string);
-        });
-    }
-
-    /** Get Javascript (does nothing)
-     * 
-     */
-    get js():Promise<execute>{
-        return new Promise((res,rej)=>res({
-            args: {},
-            function: ()=>undefined
-        }));
-    }
-
     /** Render to Element
      * 
      * Displays content after the transition out and executes javascript
@@ -77,68 +47,12 @@ export default class Countent{
      */
     public renderDisplay(target: HTMLElement): Promise<void>{
         return new Promise(async(resolve, reject)=>{
-            let content:string;
-            /** Content Time Out
-             * 
-             * Gives the content about half a second to render checking
-             * event 5 milliseconds.
-             * 
-             * @returns {string}
-             */
-            function contentTimeOut(): Promise<string> {
-                return new Promise(async(res,rej)=>{
-                    let counter = 100;
-                    while( (--counter > 0) ){
-                        if(content)
-                            res(content);
-                        await sleep(5);
-                    }
-                        
-                    rej(new Error("Content rendering has timed out!"))
-                })
+            if(this._string){
+                target.innerHTML = this._string;
+                resolve();
             }
 
-            function handleError(error: any){
-                target.ontransitionend = undefined;
-                target.innerHTML = makeErrorMessage(error, 500);
-                target.style.opacity ="";
-                reject(error);
-            }
-
-            try {
-                //Callback for when transition OUT is finished
-                target.ontransitionend = async() => {
-
-                    try {
-                        //Set content to page.
-                        target.innerHTML = await contentTimeOut();
-                        let exe:execute = await this.js;
-
-                        //Callback for when transiton IN is finished
-                        target.ontransitionend = async() => {
-
-                            target.ontransitionend = undefined;
-
-                            //Run the javascript
-                            resolve(await exe.function(exe.args));
-                        }
-
-                        //Start transition IN
-                        target.style.opacity = "";
-                    } catch(err: any){
-                        handleError(err);
-                    }
-                    
-                }
-
-                //Start transition OUT
-                target.style.opacity = "0";
-
-                //Get HTML
-                content = await this.html;
-            }catch(err: any){
-                handleError(err);
-            }
+            reject(new Error("No content to be rendered!"))
         });
     }
 

f3ec398aaa6ea579f751b42e2b090a723976b0f8
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-09-27 15:16:38 -0500

    Changes made for new Router and moved NavBar.

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,16 +1,23 @@
 import App from "./App";
+import NavBar from "./NavBar";
+
 import {Home} from "./Routes/Home";
-import {Resume} from "./Routes/Resume";
+//import {Resume} from "./Routes/Resume";
 import {Portfolio} from "./Routes/Portfolio";
 import {AboutMe} from "./Routes/AboutMe";
 
 const pkg:any = require("../../package.json");
-const app:App = new App();
 
-app.add(Home);
-app.add(Resume, true);
-app.add(Portfolio, true);
-app.add(AboutMe, true);
+const app:App = new App();
+const nav:NavBar = new NavBar();
+
+app.use("/", Home);
+//app.use("/Resume", Resume);
+app.use("/Portfolio", Portfolio);
+app.use("/About", AboutMe);
+
+nav.add(Portfolio);
+nav.add(AboutMe);
 
 app.onReady(()=>{
     console.info(`App v${pkg.version} loaded successfully!`);

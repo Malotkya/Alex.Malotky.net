d52bc8f05788361685567bf0ba0726623519b364
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-09-28 08:20:07 -0500

    Changed back.

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,5 +1,4 @@
-import App from "./App";
-import NavBar from "./NavBar";
+import App, { Context, makeErrorMessage } from "./App";
 
 import {Home} from "./Routes/Home";
 //import {Resume} from "./Routes/Resume";
@@ -9,15 +8,15 @@ import {AboutMe} from "./Routes/AboutMe";
 const pkg:any = require("../../package.json");
 
 const app:App = new App();
-const nav:NavBar = new NavBar();
 
-app.use("/", Home);
+app.add("/", Home);
 //app.use("/Resume", Resume);
-app.use("/Portfolio", Portfolio);
-app.use("/About", AboutMe);
+app.add("/Portfolio", Portfolio);
+app.add("/About", AboutMe);
 
-nav.add(Portfolio);
-nav.add(AboutMe);
+app.use(async(ctx: Context, next:Function)=>{
+    ctx.body = makeErrorMessage("Page not Found!", 404);
+});
 
 app.onReady(()=>{
     console.info(`App v${pkg.version} loaded successfully!`);

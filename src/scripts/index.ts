4bd200177f6dd9c327e707e9b13253365473d091
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-10-02 10:49:17 -0500

    Use path string in Resume Module

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -5,7 +5,7 @@
 import App, { Context, makeErrorMessage } from "./App";
 
 import {Home} from "./Routes/Home";
-import {Resume} from "./Routes/Resume";
+import {Resume, resumePath} from "./Routes/Resume";
 import {Portfolio} from "./Routes/Portfolio";
 import {AboutMe} from "./Routes/AboutMe";
 
@@ -13,7 +13,7 @@ const pkg:any = require("../../package.json");
 const app:App = new App();
 
 app.add("/", Home);
-app.add("/Resume", Resume);
+app.add(resumePath, Resume);
 app.add("/Portfolio", Portfolio);
 app.add("/About", AboutMe);
 

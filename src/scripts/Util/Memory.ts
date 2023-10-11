e9cf6dba44814c43ce78bb6b289b882c7ff3c65b
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-10-11 16:17:48 -0500

    Split cache function into two and added try/catch

--- src/scripts/Util/Memory.ts
+++ src/scripts/Util/Memory.ts
@@ -22,22 +22,41 @@ export async function cache(key: string, download: Function, timeout?:Date|numbe
     if(isNaN(update))
         update = 0;
     
-    if(update < now.valueOf()){
-        let newUpdate:Date = new Date(now.toLocaleDateString());
-        if(typeof timeout === "undefined"){
-            newUpdate.setDate(newUpdate.getDate()+1);
-        } else if(typeof timeout === "number"){
-            newUpdate = new Date(now.valueOf() + timeout);
-        } else {
-            newUpdate = timeout;
-        }
+    if(update < now.valueOf())
+        return updateLocalStorage(key, download, timeout);
 
-        const data: any = await download();
-        window.localStorage.setItem(`${key}:update`, String(newUpdate.valueOf()));
-        window.localStorage.setItem(`${key}:data`, JSON.stringify(data));
+    try {
+        return JSON.parse(window.localStorage.getItem(`${key}:data`));
+    } catch (e){
+        //JSON.parse most likely failed because localStorage was corrupted
+        return updateLocalStorage(key, download, timeout);
+    }
+}
 
-        return data;
+/** Update Local Storage
+ * 
+ * If timeout is a number, it will add that number as miliseconds to the timeout.
+ * If timeout is a date, it will set that date as the timeout.
+ * The timeout will default to tommorow if no timeout is given.
+ * 
+ * @param {string} key 
+ * @param {FUnction} download 
+ * @param {Date|number} timeout 
+ * @returns {any}
+ */
+async function updateLocalStorage(key: string, download: Function, timeout?:Date|number):Promise<any>{
+    let newUpdate:Date = new Date(Date.now());
+    if(typeof timeout === "undefined"){
+        newUpdate.setDate(newUpdate.getDate()+1);
+    } else if(typeof timeout === "number"){
+        newUpdate = new Date(newUpdate.valueOf() + timeout);
+    } else {
+        newUpdate = timeout;
     }
 
-    return JSON.parse(window.localStorage.getItem(`${key}:data`));
+    const data: any = await download();
+    window.localStorage.setItem(`${key}:update`, String(newUpdate.valueOf()));
+    window.localStorage.setItem(`${key}:data`, JSON.stringify(data));
+
+    return data;
 }

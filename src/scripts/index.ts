87c6b3e19297893bfb63dc2ae1c469fc72553862
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-10-12 13:06:04 -0500

    Commented out blog router after testing.

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -8,6 +8,7 @@ import {Home} from "./Routes/Home";
 import {Resume} from "./Routes/Resume";
 import {Portfolio} from "./Routes/Portfolio";
 import {AboutMe} from "./Routes/AboutMe";
+//import {Blog} from "./Routes/Blog";
 
 const pkg:any = require("../../package.json");
 const app:App = new App();
@@ -16,6 +17,7 @@ app.add("/", Home);
 app.add("/Resume", Resume);
 app.add("/Portfolio", Portfolio);
 app.add("/About", AboutMe);
+//app.add("/Blog", Blog);
 
 app.use(async(ctx: Context)=>{
     throw new HtmlError(404, "Page not Found!");

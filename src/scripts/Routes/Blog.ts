9c6a0ef2bcc111ebdccea8041bce31e226722667
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-10-12 13:03:54 -0500

    Changes to test reRoute.

--- src/scripts/Routes/Blog.ts
+++ src/scripts/Routes/Blog.ts
@@ -2,18 +2,44 @@
  * 
  * @author Alex Malotky
  */
-import {Context, Router, render, sleep} from "../App"
+import {Context, Router, render} from "../App";
+import Database from "../Util/Database";
+import { cache } from "../Util/Memory";
+
+const PAGENATION_SIZE = 25;
 
 /** Blog Module
  * 
  */
 export const Blog = new Router("Blog", "A Blog from Alex.");
-Blog.path = "/Blog/:page?";
 
-Blog.use(async(ctx: Context)=>{
-    let page: number = Number(ctx.params.get("page"));
-    if(isNaN(page))
-        page = 0;
+Blog.use("/:page?",async(ctx: Context)=>{
+    //const database = await Database();
 
-    ctx.body = await render("");
+    let page: string = ctx.params.get("page");
+    let currentPage: number = Number(page);
+    if(page === "undefined")
+        currentPage = 0;
+
+    let maxPages: number = 5; //Math.floor((await database.countCollection("Blog")) / PAGENATION_SIZE);
+
+    if(isNaN(currentPage) || currentPage < 0) {
+        return ctx.reRoute("/Blog");
+    } else if(currentPage > maxPages){
+        return ctx.reRoute(`/Blog/${maxPages}`);
+    }
+
+    ctx.body = "Comming Soon!";
+
+    /* const results = await database.queryCollection("Blog", {
+        orderBy: ["postDate", "desc"],
+        startAt: [currentPage * PAGENATION_SIZE],
+        limit: [PAGENATION_SIZE]
+    });
+ 
+    ctx.body = await render("", {
+        results: results,
+        page: currentPage+1,
+        pageCount: maxPages
+    });*/
 });

e349080c5b8840a90e6cfca6228ff07f6adebf5f
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-10-24 14:42:15 -0500

    Testing new elements.

--- src/static/scripts/mtg/edit.js
+++ src/static/scripts/mtg/edit.js
@@ -1,8 +1,8 @@
-import {createDeckFromString} from "/scryfall.js";
+import * as Scryfall from "/scryfall.js";
 import {Database} from "/firebase.js";
 
 export default function Edit(){
-    document.querySelector("form").addEventListener("submit", performEdit);
+    document.querySelector("#submit").addEventListener("click", performEdit);
     document.querySelector("#btnView").addEventListener("click", ()=>{
         localStorage.clear();
     });
@@ -20,54 +20,52 @@ function performEdit(event){
 
         const name = document.querySelector("#name").value;
         const description = document.querySelector("#description").value;
-        const deckList = document.querySelector("#deckList").value;
 
-        createDeckFromString(deckList).then(deck=>{
+        const deck = document.querySelector("#deckList").getDeckObject();
 
-            deck.name = name;
-            deck.description = description;
+        deck.name = name;
+        deck.description = description;
                 
-            const identity = new Set();
-            deck.commanders.forEach(card=>{
-                const buffer = JSON.stringify(card);
+        const identity = new Set();
+        deck.commanders.forEach(card=>{
+            const buffer = JSON.stringify(card);
 
-                //White
-                if(buffer.match(/{.{0,4}W{1}.{0,4}}/g))
-                    identity.add("white");
+            //White
+            if(buffer.match(/{.{0,4}W{1}.{0,4}}/g))
+                identity.add("white");
 
-                //Blue
-                if(buffer.match(/{.{0,4}U{1}.{0,4}}/g))
-                    identity.add("blue");
+            //Blue
+            if(buffer.match(/{.{0,4}U{1}.{0,4}}/g))
+                identity.add("blue");
 
-                //Black
-                if(buffer.match(/{.{0,4}B{1}.{0,4}}/g))
-                    identity.add("black");
+            //Black
+            if(buffer.match(/{.{0,4}B{1}.{0,4}}/g))
+                identity.add("black");
 
-                //Red
-                if(buffer.match(/{.{0,4}R{1}.{0,4}}/g))
-                    identity.add("red");
+            //Red
+            if(buffer.match(/{.{0,4}R{1}.{0,4}}/g))
+                identity.add("red");
 
-                //Green
-                if(buffer.match(/{.{0,4}G{1}.{0,4}}/g))
-                    identity.add("green");
-            });
+            //Green
+            if(buffer.match(/{.{0,4}G{1}.{0,4}}/g))
+                identity.add("green");
+        });
 
-            deck.color_identity = [...identity];
-            if(deck.commanders.length > 0){
-                if(deck.commanders[0].art)
-                    deck.art = deck.commanders[0].art;
-            }
+        deck.color_identity = [...identity];
+        if(deck.commanders.length > 0){
+            if(deck.commanders[0].art)
+                deck.art = deck.commanders[0].art;
+        }
             
 
-            //Submit here instead becasue can't use routing option.
-            Database.updateDocument("MtgDecks", id, deck).then(()=>{
-                alert("Success");
-            }).catch(e=>{
-                throw e;
-            });
+        //Submit here instead becasue can't use routing option.
+        /*Database.updateDocument("MtgDecks", id, deck).then(()=>{
+            alert("Success");
         }).catch(e=>{
             throw e;
-        });
+        });*/
+        console.log(deck);
+        alert("Success");
 
     } catch (e){
         console.error(e);

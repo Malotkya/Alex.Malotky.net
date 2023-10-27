2e49645748ee8e7406d8134821055df120ac8ac1
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-10-27 16:38:18 -0500

    Function to bundle route modules together.

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,14 @@ const TerserPlugin = require("terser-webpack-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const SpliceWebpackPlugin = require("./util/SpliceWebpackPlugin.js");
+const SpliceWebpackPlugin = require("./webpack/SpliceWebpackPlugin.js");
+const TemplateBuilder = require("./webpack/TemplateBundler.js");
 
 //directories used multiple times
 const build_directory = path.resolve(__dirname, 'build');
 const source_directory = path.resolve(__dirname, "src");
+
+const bundleTemplates = TemplateBuilder(build_directory, source_directory);;
 
 //test if in production environment
 const prod = process.argv.includes('prod');
@@ -27,11 +30,11 @@ module.exports = {
   mode: prod? "production": "development",
   entry: {
     script: [
-      path.join(source_directory, 'scripts', 'index.ts'),
-      path.join(source_directory, 'styles', 'index.scss')
+      path.join(source_directory, 'index.ts'),
+      path.join(source_directory, 'index.scss')
     ],
-    firebase: path.join(source_directory, "scripts", "firebase.ts"),
-    scryfall: path.join(source_directory, "scripts", "scryfall.ts")
+    firebase: path.join(source_directory, "firebase.ts"),
+    scryfall: path.join(source_directory, "scryfall.ts")
   },
   devtool: prod? undefined: 'source-map',
   module: {
@@ -69,20 +72,12 @@ module.exports = {
   },
   plugins: [
     new CopyWebpackPlugin({
-      patterns: [
+      patterns: bundleTemplates("routes") .concat([
         {
           to: build_directory,
           from: path.join(source_directory, 'static')
         }
-      ]
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          to: path.join(build_directory, 'templates'),
-          from: path.join(source_directory, 'views')
-        }
-      ]
+      ])
     }),
     new SpliceWebpackPlugin()
   ],
---
+++ webpack/TemplateBundler.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const path = require("path");
+
+function TemplateBundler(build, source){
+    return (dir) => {
+        const output = [];
+        for(let module of fs.readdirSync( path.join(source, dir))){
+            const directory = path.join(source, dir, module);
+            for(let file of fs.readdirSync(directory)){
+                switch (file){
+                    case "static":
+                        output.push({
+                            to: build,
+                            from: path.join(directory, file)
+                        });
+                        break;
+                    
+                    case "views":
+                    case "view":
+                        output.push({
+                            to: path.join(build, "templates"),
+                            from: path.join(directory, file)
+                        });
+                        break;
+
+                    case "index.html":
+                        output.push({
+                            to: path.join(build, "templates", module+".html"),
+                            from: path.join(directory, file)
+                        });
+                        break;
+                }
+            }
+        }
+
+        return output;
+    }
+}
+
+module.exports = TemplateBundler;

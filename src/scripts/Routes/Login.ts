518efb42a93162f3d51caf2d06674b562c362a68
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-10-27 11:15:56 -0500

    Login tool using rerouting.

--- src/scripts/Routes/Login.ts
+++ src/scripts/Routes/Login.ts
@@ -5,13 +5,33 @@ export const Login = new Router("Login");
 
 Login.use("/Login", async(ctx: Context)=>{
     const auth = await Authentication();
+    let error: string = "";
 
     //Go Back Home
     if(await auth.getCurrentUser())
-        return ctx.reRoute("/");
+        return ctx.reRoute("back");
+
+    const username:string = ctx.params.get("username") || "";
+    const password:string = ctx.params.get("password") || "";
+
+    if(username !== "" || password !== ""){
+        try {
+            if(await auth.signInUser(username, password)) {
+                return ctx.reRoute("back");
+            } else {
+                error = "Unable to login!";
+            }
+        } catch (e){
+            error = "Wrong Username or Password!";
+        }
+    }
 
     ctx.connected = await execute("login.js");
-    ctx.body = await render("login.html");
+    ctx.body = await render("login.html", {
+        username: username,
+        password: password,
+        error: error
+    }); 
 });
 
 Login.use("/Logout", async(ctx:Context)=>{
--- src/static/scripts/login.js
+++ src/static/scripts/login.js
@@ -1,39 +1,7 @@
-import { Authentication } from "/firebase.js";
-
 export default function LogIn() {
-    document.querySelector("form").addEventListener("submit", performLogin);
-}
-
-function performLogin(event){
-    event.preventDefault();
-
-    const username = event.target.querySelector("#username").value.trim();
-    const password = event.target.querySelector("#password").value.trim();
-    
-    if(username === "") {
-        printErrorMessage("Please Enter a Username!");
-    } else if(password === "") {
-        printErrorMessage("Please Enter a Password!")
-    } else {
-        Authentication.signInUser(username, password).then(user=>{
-            if(user) {
-                location.reload();
-            } else {
-                printErrorMessage("Unable to login!");
-            }
-        }).catch(err =>{
-            printErrorMessage("Wrong Username or Password!");
-        })
-    }
-
-    return false;
-}
-
-function printErrorMessage(message){
-    const htmlMessage = document.querySelector("#errorMessage");
-
-    if(htmlMessage)
-        htmlMessage.textContent = message;
-
-    console.error(message);
+    document.querySelector("form").addEventListener("submit", (event)=>{
+        event.preventDefault();
+        window.route(location.pathname, new FormData(event.target));
+        return false;
+    });
 }

b87ff3a9507aeb6ced64dc7181497a6a2a0d6b95
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-10-27 14:23:53 -0500

    Using custom elements

--- src/static/scripts/mtg/deck.js
+++ src/static/scripts/mtg/deck.js
@@ -1,14 +1,6 @@
-import {Masonry} from "/masonry.js"
+import * as Scryfall from "/scryfall.js"
 
 export default function Deck(){
-
-    //Masonry Magic
-    new Masonry('#masonry-layout', {
-        itemSelector: '.category',
-        columnWidth: '.category',
-        percentPosition: true
-    }).layout();
-
     //Needed for apple devices
     document.querySelectorAll(".card").forEach(card=>{
         card.addEventListener("click", event=>{

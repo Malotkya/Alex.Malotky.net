c05c0b6d2549628458b89adbce51f19ce1caa919
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-12-22 14:01:20 -0600

    Added more content to pokemon module.

--- src/routes/pokemon/module.ts
+++ src/routes/pokemon/module.ts
@@ -1,7 +1,15 @@
+import { createElement } from "../../util/Elements";
 import PokemonGameElement from "./content/PokemonGameElement";
 
-export const content:Array<PokemonGameElement> = [
-    new PokemonGameElement(require("./games/yellow.json")),
-    new PokemonGameElement(require("./games/crystal.json")),
-    new PokemonGameElement(require("./games/ruby.json"))
-];
+export default function Pokemon() {
+    return createElement("body-element", 
+        createElement("h2", "Pokemon Game Marathon"),
+        createElement("aside", {id:"about"},
+            createElement("h3", "About:"),
+            createElement("p", "I have recently got back into playing the mainline pokemon games.  I haven't played any of them sence Generation 3, so I have accquire all the games and have been playing them in order.  Bellow are the list of pokemon that I used in each games along with my thoughts about each game.")
+        ),
+        new PokemonGameElement(require("./games/yellow.json")),
+        new PokemonGameElement(require("./games/crystal.json")),
+        new PokemonGameElement(require("./games/ruby.json"))
+    );
+};

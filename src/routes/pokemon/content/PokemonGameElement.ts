5e9c24abc7b0ab34bff73b2914abe16b7cbd99ff
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-12-28 10:18:37 -0600

    Using new elements

--- src/routes/pokemon/content/PokemonGameElement.ts
+++ src/routes/pokemon/content/PokemonGameElement.ts
@@ -1,8 +1,10 @@
 import PokemonElement, {PokemonType} from "./PokemonElement";
+import PokemonTeamViewElement from "./PokemonTeamViewElement";
 import { createElement as _ } from "../../../util/Elements";
 
 export interface PokemonGameType {
     game: string,
+    version: string,
     generation: number,
     region: string,
     team: Array<PokemonType>,
@@ -14,8 +16,7 @@ export default class PokemonGameElement extends HTMLElement {
     private _name:HTMLHeadElement;
     private _info:HTMLElement;
     private _comments: HTMLElement; 
-    private _team: HTMLElement;
-    private _others: HTMLElement|null;
+    private _team: PokemonTeamViewElement;
 
     constructor(data:PokemonGameType){
         super();
@@ -23,16 +24,16 @@ export default class PokemonGameElement extends HTMLElement {
         this._name = _("h3", {class: "game-name"}, data.game);
         this._info = _("p", {class: "game-info"}, `Generation: ${data.generation}<br/> Region: ${data.region}`);
         this._comments = _("p", {class: "game-comments"}, Array.isArray(data.comments)? data.comments.join(): data.comments);
-        this._team = _("section", {id: "game-team"}, data.team.map(pokemon=>new PokemonElement(pokemon)));
-        this._others = data.others.length === 0? null: _("section", {id: "game-others"}, data.others.map(pokemon=> new PokemonElement(pokemon)));
+        this._team = new PokemonTeamViewElement(
+            data.team.map(pokemon=>new PokemonElement(pokemon, data.version)),
+            data.others.map(pokemon=> new PokemonElement(pokemon, data.version))
+        );
     }
 
     connectedCallback(){
         this.appendChild(this._name);
         this.appendChild(this._info);
         this.appendChild(this._team);
-        if(this._others !== null)
-            this.appendChild(this._others);
         this.appendChild(this._comments);
     }
 }

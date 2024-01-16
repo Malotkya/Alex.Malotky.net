ffe8d38985eab6e5a5f86c8483684dd055554c82
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-01-16 16:15:36 -0600

    Added Ultra Sun Game to content

--- src/routes/pokemon/module.ts
+++ src/routes/pokemon/module.ts
@@ -49,13 +49,14 @@ export default function Pokemon(init?:string):Content {
             _("h3", "About:"),
             _("p", "I have recently got back into playing the mainline pokemon games.  I haven't played any of them sence Generation 3, so I have accquire all the games and have been playing them in order.  Bellow are the list of pokemon that I used in each games along with my thoughts about each game.")
         ),
-        gameSelect(init || "x", {
+        gameSelect(init || "sun", {
             "yellow"  : require("./games/yellow.json"),
             "crystal" : require("./games/crystal.json"),
             "ruby"    : require("./games/ruby.json"),
             "platinum": require("./games/platinum.json"),
             "white"   : require("./games/white.json"),
-            "x"       : require("./games/x.json")
+            "x"       : require("./games/x.json"),
+            "sun"     : require("./games/sun.json")
         })
     ];
 };

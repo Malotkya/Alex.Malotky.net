1c933d820c744cd750d47f216a46c1f616b640ac
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-01-16 08:02:40 -0600

    Removed comments from displayed content.

--- src/routes/pokemon/content/PokemonGameElement.ts
+++ src/routes/pokemon/content/PokemonGameElement.ts
@@ -9,13 +9,11 @@ export interface PokemonGameType {
     region: string,
     team: Array<PokemonType>,
     others: Array<PokemonType>,
-    comments: Array<string>|string
 }
 
 export default class PokemonGameElement extends HTMLElement {
     private _name:HTMLHeadElement;
     private _info:HTMLElement;
-    private _comments: HTMLElement; 
     private _team: PokemonTeamViewElement;
 
     constructor(data:PokemonGameType){
@@ -23,7 +21,6 @@ export default class PokemonGameElement extends HTMLElement {
 
         this._name = _("h3", {class: "game-name"}, data.game);
         this._info = _("p", {class: "game-info"}, `Generation: ${data.generation}<br/> Region: ${data.region}`);
-        this._comments = _("p", {class: "game-comments"}, Array.isArray(data.comments)? data.comments.join(): data.comments);
         this._team = new PokemonTeamViewElement(
             data.team.map(pokemon=>new PokemonElement(pokemon, data.version, data.game)),
             data.others.map(pokemon=> new PokemonElement(pokemon, data.version, data.game))
@@ -33,7 +30,6 @@ export default class PokemonGameElement extends HTMLElement {
     connectedCallback(){
         this.appendChild(this._name);
         this.appendChild(this._info);
-        this.appendChild(this._comments);
         this.appendChild(this._team);
     }
 }

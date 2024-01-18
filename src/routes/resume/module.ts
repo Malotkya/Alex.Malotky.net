e1240e87b7b6551ac6f4bac304649c5e586ab8d6
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-01-18 15:58:41 -0600

    Commented code and adjusted args to make them more clear and style fixes.

--- src/routes/resume/module.ts
+++ src/routes/resume/module.ts
@@ -3,25 +3,85 @@ import {SkillItem, SkillCard, SkillDetailed} from "./content/SkillElements";
 import {JobItem, JobCard, JobDetailed} from "./content/JobElements";
 import {SchoolItem, SchoolCard, SchoolDetailed} from "./content/SchoolElements";
 
+/** Resume Skills Module
+ * 
+ * @param {Array<SkillItem>|SkillItem} args 
+ * @returns {Content}
+ */
+export function Skills(args:Array<SkillItem>|SkillItem): Content{
+    if(Array.isArray(args)){
+        return [
+            _("h1", "Skills:"),
+            _("ul", {class: "resume-card-list"},
+                args.map(SkillCard)
+            )
+        ]
+    }
+
+    return SkillDetailed(args);
+}
+
+/** Resume School Module
+ * 
+ * @param {Array<SchoolItem>|SchoolItem} args 
+ * @returns {Content}
+ */
+export function School(args:Array<SchoolItem>|SchoolItem): Content{
+    if(Array.isArray(args)){
+        return [
+            _("h1", "School:"),
+            _("ul", {class: "resume-card-list"},
+                 args.map(SchoolCard)
+            )
+        ]
+    }
+
+    return SchoolDetailed(args);
+}
+
+/** Resume Job Module
+ * 
+ * @param {Array<JobItem>|JobItem} args 
+ * @returns {Content}
+ */
+export function Jobs(args:Array<JobItem>|JobItem): Content{
+    if(Array.isArray(args)){
+        return [
+            _("h1", "Work History:"),
+            _("ul", {class: "resume-card-list"},
+                args.map(JobCard)
+            )
+        ]
+    }
+
+    return JobDetailed(args);
+}
+
+/** Main Resume Module
+ * 
+ * @param args 
+ * @returns 
+ */
 export default function Resume(args?: any): Content{
     switch(args.type) {
         case "Jobs":
-            return Jobs(args.arr);
+            return Jobs(args.result);
 
         case "School":
-            return School(args.arr);
+            return School(args.result);
 
         case "Skills":
-            return Skills(args.arr);
+            return Skills(args.result);
     }
 
+    //If no type then assume is default page.
     return [
         _("h1", "Resume"),
         _("article",
             _("h2", 
-                _("a", {href: "/Resume/Jobs"}, "Job History:")
+                _("a", {href: "/Resume/Jobs"}, "Work History:")
             ),
-            _("ul", {class: "resume-card-list"},
+            _("ul", {class: "resume-card-list compact-resume"},
                 args.jobs.map(JobCard)
             )
         ),
@@ -29,7 +89,7 @@ export default function Resume(args?: any): Content{
             _("h2", 
                 _("a", {href: "/Resume/School"}, "School History:")
             ),
-            _("ul", {class: "resume-card-list"},
+            _("ul", {class: "resume-card-list compact-resume"},
                 args.schools.map(SchoolCard)
             )
         ),
@@ -37,42 +97,9 @@ export default function Resume(args?: any): Content{
             _("h2", 
                 _("a", {href: "/Resume/Jobs"}, "Skills:")
             ),
-            _("ul", {class: "resume-card-list"},
+            _("ul", {class: "resume-card-list compact-resume"},
                 args.skills.map(SkillCard)
             )
         ),
     ]
-}
-
-export function Skills(args:Array<SkillItem>|SkillItem): Content{
-    if(Array.isArray(args)){
-        return [
-            _("h1", "Skills:"),
-            _("ul", args.map(SkillCard))
-        ]
-    }
-
-    return SkillDetailed(args);
-}
-
-export function School(args:Array<SchoolItem>|SchoolItem): Content{
-    if(Array.isArray(args)){
-        return [
-            _("h1", "Skills:"),
-            _("ul", args.map(SchoolCard))
-        ]
-    }
-
-    return SchoolDetailed(args);
-}
-
-export function Jobs(args:Array<JobItem>|JobItem): Content{
-    if(Array.isArray(args)){
-        return [
-            _("h1", "Skills:"),
-            _("ul", args.map(JobCard))
-        ]
-    }
-
-    return JobDetailed(args);
 }

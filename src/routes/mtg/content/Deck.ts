bf31af1b3a6d5b90f7df56ef76da0670ca5d30fe
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-01-19 09:49:10 -0600

    Added Comments

--- src/routes/mtg/content/Deck.ts
+++ src/routes/mtg/content/Deck.ts
@@ -1,15 +1,25 @@
-import {createElement as _} from "../../../util/Elements";
-import DeckViewElement from "./elements/DeckView";
-import DeckEditorElement, {Deck} from "./elements/DeckEditor";
-import CardElement from "./elements/View/Card";
 
+/** /routes/mtg/content/Deck.ts
+ * 
+ * @author Alex Malotky
+ */
+import {createElement as _, Content} from "../../../util/Elements";
+import DeckViewElement, {CardElement} from "./elements/DeckView";
+import DeckEditorElement, {Deck} from "./elements/DeckEditor";
+
+//Extra deck information in database.
 export interface DeckItem extends Deck {
     id: string,
     name: string,
     description: string
 };
 
-export function DeckView(deck:DeckItem){
+/** Deck View Module
+ * 
+ * @param {DeckItem} deck 
+ * @returns {Content}
+ */
+export function DeckView(deck:DeckItem): Content{
     return [
         _("aside", {id:"deckHeader"},
             _("h1", deck.name),
@@ -19,14 +29,19 @@ export function DeckView(deck:DeckItem){
         _("article", {id: "deck-view"}, 
             _("section", {id:"commanders", class: "category"},
                 _("h2", deck.commanders.length > 2? "Commanders:": "Commander:"),
-                _("ul", deck.commanders.map(c=> new CardElement(c)))
+                _("ul", deck.commanders.map(c=> CardElement(c)))
             ),
             new DeckViewElement(deck.main_deck)
         )
     ]
 }
 
-export function DeckEdit(deck:DeckItem){
+/** Deck Edit Module
+ * 
+ * @param {DeckItem} deck 
+ * @returns {Content}
+ */
+export function DeckEdit(deck:DeckItem): Content{
     return _("article", {id:"deckEditor"},
         _("a", {href: "/Decks/Editor", class:"btn"}, "Back"),
         _("a", {href: `/Decks/${deck.id}`, id: "btnView", class: "btn"}, "View"),

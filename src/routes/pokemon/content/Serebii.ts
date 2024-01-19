59fc85f2685232e20b10aead00d887c15de6305b
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-01-19 10:43:00 -0600

    Serebii information in one spot.

---
+++ src/routes/pokemon/content/Serebii.ts
@@ -0,0 +1,40 @@
+import { Pokemon } from "./PokemonTypes";
+
+const SEREBII_URI = "https://www.serebii.net/";
+const MASTER_POKEMON_LIST: Array<string> = require("../../../../pokemon.json");
+
+
+/** Get Pokemon Number from Name
+ * 
+ * @param {string} name 
+ * @returns {number}
+ */
+function getNumber(name:string):number{
+    return MASTER_POKEMON_LIST.indexOf(name) + 1;
+}
+
+/** Format Image URI
+ * 
+ * @param {Pokemon} pokemon - Pokemon Information
+ * @param {StringIndex} version - Game Version Information
+ * @returns {string}
+ */
+export function formatURI(pokemon:Pokemon, version:StringIndex = {}):string {
+    const {
+        normal = "pokemon/art/",
+        shiney = "pokemon/art/",
+        override = ".png"
+    } = version;
+
+    let number:number|string = getNumber(pokemon.name);
+    if(number < 1000){
+        number = `00${number}`.slice(-3);
+    }
+
+    return SEREBII_URI.concat(
+        pokemon.shiney? shiney: normal,
+        `/${number}`,
+        pokemon.modifier || "",
+        override
+    );
+}

e725daa9180674ca547e4fdd7cb494f0b1aba7ab
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-01-25 11:20:07 -0600

    Changes to importing firebase from firebase module.

--- src/util/Authentication.ts
+++ src/util/Authentication.ts
@@ -2,21 +2,15 @@
  * 
  * @author Alex Malotky
  */
-import { User } from "../firebase";
+import { User, Authentication } from "../firebase";
 export { User } from "../firebase";
-
-interface Authentication {
-    signInUser(email:string, password:string):Promise<User>,
-    signOutUser():Promise<void>,
-    getCurrentUser():Promise<User>
-}
 
 let authentication:Authentication;
 
-export default async function Authentication(): Promise<Authentication>{
+export default async function init(): Promise<Authentication>{
     if(typeof authentication === "undefined") {
         //@ts-ignore
-        authentication = (await import(/*webpackIgnore: true*/ "/firebase.js")).Authentication;
+        authentication = (await import(/*webpackIgnore: true*/ "/firebase.js")).default("auth");
     }
 
     return authentication;
--- src/util/Database.ts
+++ src/util/Database.ts
@@ -2,20 +2,8 @@
  * 
  * @author Alex Malotky
  */
-import {Timestamp} from "../firebase";
-
-/** Database Interface
- * 
- */
-export interface Database{
-    queryCollection(collectionName:string, opts?:any):Promise<Array<any>>,
-    getDocument(collectionName:string, documentId:string):Promise<any>,
-    countCollection(collectionName:string):Promise<number>
-    updateDocument(collectionName:string, documentId:string, object:any):Promise<void>,
-    deleteDocument(collectionName:string, documentId:string):Promise<void>,
-    createDocument(collectionName:string, object:any):Promise<string>,
-    now():Promise<Timestamp>
-}
+import {Timestamp, Database} from "../firebase";
+export {Timestamp};
 
 //Database object.
 let database: Database;
@@ -26,10 +14,10 @@ let database: Database;
  * 
  * @returns {Database}
  */
-export default async function Database(): Promise<Database>{
+export default async function init(): Promise<Database>{
     if(typeof database === "undefined") {
         //@ts-ignore
-        database = (await import(/*webpackIgnore: true*/ "/firebase.js")).Database;
+        database = (await import(/*webpackIgnore: true*/ "/firebase.js")).default("database");
     }
 
     return database;

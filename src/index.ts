7524f35a54c224e49153e48b67802f5da6132d81
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-04-18 09:53:16 -0500

    created app wireframe.

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import {App, Context, View, Content} from "zim-engine";
+import Template, {NavLink, ErrorContent} from "./util/Templates";
+
+import {Home, HOME_TITLE} from "./routes/home";
+
+const app = new App();
+const navBar:Array<Content> = [];
+
+app.view(new View(
+    [
+        {name: "charset", content:"UTF-8"},
+        {name: "viewport", content: "width=device-wdith, initial-scale=1"},
+        {name: "author", content: "Alex Malotky"},
+        {name: "title", content: "Alex.Malotky.Net"},
+        {name: "description", content: "Portfolio website for Alex Malotky."},
+        {name: "link", attributes: {rel: "stylesheet", href: "/style.css"}}
+    ],
+    (c:Content)=>Template(navBar, c),
+    {
+        lang:"en",
+        dir:"ltr"
+    }
+));
+
+app.errorHandler((err:any, ctx:Context)=>{
+    const {
+        status = 500,
+        message = err || "An Unknown Error Occured!"
+    } = err;
+
+    ctx.status(status);
+    const type = ctx.request.headers.get("content-type");
+    if(type && type.includes("json")) {
+        ctx.json({status, message});
+    } else {
+        ctx.render(ErrorContent(status, message));
+    }
+});
+
+navBar.push(NavLink("Home", HOME_TITLE));
+app.use("/", Home);
+
+export default app;
---
+++ src/util/Templates.ts
@@ -0,0 +1,68 @@
+import { Content, createElement as _, RenderUpdate} from "zim-engine/lib/View";
+export {Content, _};
+
+export default function WireFrame(navList:Content, content:Content):Content{
+    return [
+        _("header", 
+            _("a", {class: "skip", href:"#main"}, "Skip Link"),
+            Navigation(navList),
+        ),
+        _("main", content),
+        Footer()
+    ]
+}
+
+export function NavLink(href:string, title:string):Content{
+    return  _("li",
+        _("a", {class: "top-nav-item", href:href}, title)
+    );
+}
+
+export function HamburgerButton():Content {
+    return _("button",
+        {
+            id: "top-nav-button",
+            "aria-label": "Show/Hide Navigation",
+            "aria-haspopup": "menu"
+        },
+        _("div"),
+        _("div"),
+        _("div")
+    );
+}
+
+function Navigation(list:Content):Content {
+    return _("nav", {id:"top-nav-bar"},
+        _("a", {id: "top-nav-title", class: "top-nav-item", href: "/"}, "Alex.Malotky.net"),
+        HamburgerButton(),
+        _("ul", {id: "top-nav-menu"}, list)
+    );
+}
+
+function Footer():Content {
+    return _("footer", 
+        _("p", _("strong", "Where to find me:")),
+        _("p",
+            _("a", {href:"https://github.com/Malotkya", target:"_blank"},
+                _("img", {src: "/media/github.svg", alt:"GitHub", class:"icon"}, true )
+            ),
+            _("a", {href:"https://www.linkedin.com/in/amalotky/", target:"_blank"},
+                _("img", {src: "/media/linkedin.svg", alt:"LinkedIn", class:"icon"}, true )
+            ),
+            _("a", {href:"mailto: Malotkya@outlook.com"},
+                _("img", {src: "/media/email.png", alt:"Email", class:"icon"}, true )
+            )
+        )
+    )
+}
+
+export function ErrorContent(status:number, message:string):RenderUpdate {
+    const string = `${status}: ${message}`;
+    const header = {
+        title: string
+    };
+
+    const content:Content = _("h2", {class:"error"}, string);
+
+    return {header, content};
+}

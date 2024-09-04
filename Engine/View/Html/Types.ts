405cb548082645884c07d2a158b3fd46e29163a1
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-09-04 11:40:37 -0500

    Moved last of types to other files.

--- Engine/View/Html/Types.ts
+++
@@ -1,7 +0,0 @@
-export type HTMLContent = string;
-
-// Relation Types
-// https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/rel
-export type LinkRel   = "alternate"|"author"|"canonical"|"dns-prefetch"|"expect"|"help"|"icon"|"manifest"|"modulepreload"|"next"|"pingback"|"preconnect"|"prefetch"|"preload"|"prerender"|"prev"|"privacy-policy"|"search"|"stylesheet"|"terms-of-service";
-export type FormRel   = "external"|"help"|"lisence"|"next"|"nofollow"|"noopener"|"noreffer"|"prev"|"search";
-

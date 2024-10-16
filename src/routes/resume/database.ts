0afa07455f2a0ee6b2e800ea84e10ffb1e0f1cf1
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-10-16 10:27:28 -0500

    Moved database call functions to their own file, and fixed confusing double function.

---
+++ src/routes/resume/database.ts
@@ -0,0 +1,154 @@
+import { validateSchoolItem } from "./view/school";
+import { validateJobItem } from "./view/job";
+import { validateSkillItem } from "./view/skill";
+
+/** Query Wrapper
+ * 
+ * @param db 
+ * @param query 
+ * @returns 
+ */
+export async function queryWrapper(db:D1Database, query:string):Promise<Record<string, unknown>[]> {
+    const {results, error} = await db.prepare(query).all();
+
+    if(error)
+        throw error;
+
+    return results;
+}
+
+export const TABLES:Dictionary<string|undefined> = {
+    "jobs"  : "SELECT * FROM Jobs ",
+    "skills": "SELECT * FROM Skills ",
+    "school": "SELECT * FROM School "
+}
+export const ORDER_BY:Dictionary<string> = {
+    "jobs"  : " ORDER BY startDate DESC",
+    "skills": "",
+    "school": " ORDER BY graduated DESC"
+}
+export const DELETE:Dictionary<string|undefined> = {
+    "jobs"  :"DELETE FROM Jobs WHERE id = ?",
+    "skills":"DELETE FROM Skills WHERE id = ?",
+    "school":"DELETE FROM School WHERE id = ?"
+}
+
+/** Update or Insert Query
+ * 
+ * @param {string} table 
+ * @param {Map} data 
+ * @param {string|number} id (stirng = update / number = insert)
+ * @returns {{query:string|undefined, data:Array<unknown>, result:Record<string, unknown>}}
+ */
+export function UPDATE_QUERY(table:string, data:Map<string, string>, id:string):{query:string|undefined, data:Array<unknown>, result:Record<string, unknown>} {
+    let name:string|undefined;
+    switch (table){
+        case "jobs":
+            const employer = data.get("employer");
+            const startDate = data.get("startDate");
+            const endDate = data.get("endDate");
+            const title = data.get("title");
+            const about = data.get("about");
+
+            const job = {employer, startDate, endDate, title, about, id}
+            validateJobItem(job, false);
+            return {
+                query: "INSERT INTO Jobs(employer, startDate, endDate, title, about, id) VALUES(?, ?, ?, ?, ?, ?)",
+                data: [employer, startDate, endDate, title, about, id],
+                result: job
+            }
+
+        case "school":
+            name = data.get("name");
+            const graduated = data.get("graduated");
+            const degree = data.get("degree");
+            const other = data.get("other");
+
+            const school = {name, graduated, degree, other, id}
+            validateSchoolItem(school, false);
+            return {
+                query: "UPDATE School SET name = ? graduated = ? degree = ? other = ? WHERE id = ?",
+                data: [name, graduated, degree, other, id],
+                result: school
+            }
+
+        case "skills":
+            name = data.get("name");
+            const list = data.get("list");
+            const info = data.get("info");
+            
+            const skill = {name, list, info, id};
+            validateSkillItem(skill, false);
+            return {
+                query: "UPDATE Skills SET name = ? list = ? info = ? WHERE id = ?",
+                data: [name, list, info, id],
+                result: skill
+            }
+    }
+
+    return {
+        query: undefined,
+        data: [],
+        result: {}
+    }
+}
+
+/** Insert Query
+ * 
+ * @param {string} table 
+ * @param {Map} data 
+ * @param {number} id
+ * @returns {{query:string|undefined, data:Array<unknown>, result:Record<string, unknown>}}
+ */
+export function INSERT_QUERY(table:string, data:Map<string, string>, id:number):{query:string|undefined, data:Array<unknown>, result:Record<string, unknown>} {
+    let name:string|undefined;
+    switch (table){
+        case "jobs":
+            const employer = data.get("employer");
+            const startDate = data.get("startDate");
+            const endDate = data.get("endDate");
+            const title = data.get("title");
+            const about = data.get("about");
+
+            const job = {employer, startDate, endDate, title, about, id}
+            validateJobItem(job, false);
+            return {
+                query: "INSERT INTO Jobs(employer, startDate, endDate, title, about, id) VALUES(?, ?, ?, ?, ?, ?)",
+                data: [employer, startDate, endDate, title, about, id],
+                result: job
+            }
+
+        case "school":
+            name = data.get("name");
+            const graduated = data.get("graduated");
+            const degree = data.get("degree");
+            const other = data.get("other");
+
+            const school = {name, graduated, degree, other, id}
+            validateSchoolItem(school, false);
+            return {
+                query: "INSERT INTO School(name, graduated, degree, other, id) VALUES(?, ?, ?, ?, ?)",
+                data: [name, graduated, degree, other, id],
+                result: school
+            }
+
+        case "skills":
+            name = data.get("name");
+            const list = data.get("list");
+            const info = data.get("info");
+            
+            const skill = {name, list, info, id};
+            validateSkillItem(skill, false);
+            return {
+                query: "INSERT INTO Jobs(name, list, info, id) VALUES(?, ?, ?, ?)",
+                data: [name, list, info, id],
+                result: skill
+            }
+    }
+
+    return {
+        query: undefined,
+        data: [],
+        result: {}
+    }
+}
--- src/routes/resume/index.ts
+++ src/routes/resume/index.ts
@@ -8,108 +8,7 @@ import ResumeView , { SingleView, ListView, SingleEditView, ListEditView, EditMa
 import { validateSchoolItem } from "./view/school";
 import { validateJobItem } from "./view/job";
 import { validateSkillItem } from "./view/skill";
-
-const TABLES:Dictionary<string|undefined> = {
-    "jobs"  : "SELECT * FROM Jobs ",
-    "skills": "SELECT * FROM Skills ",
-    "school": "SELECT * FROM School "
-}
-const ORDER_BY:Dictionary<string> = {
-    "jobs"  : " ORDER BY startDate DESC",
-    "skills": "",
-    "school": " ORDER BY graduated DESC"
-}
-const DELETE:Dictionary<string|undefined> = {
-    "jobs"  :"DELETE FROM Jobs WHERE id = ?",
-    "skills":"DELETE FROM Skills WHERE id = ?",
-    "school":"DELETE FROM School WHERE id = ?"
-}
-const NEW:Dictionary<string|undefined> = {
-    "jobs"  :"INSERT INTO Jobs(id) VALUES(?)",
-    "skills":"INSERT INTO Skills(id) VALUES(?)",
-    "school":"INSERT INTO School(id) VALUES(?)"
-}
-
-/** Update or Insert Query
- * 
- * @param {string} table 
- * @param {Map} data 
- * @param {string|number} id (stirng = update / number = insert)
- * @returns {{query:string|undefined, data:Array<unknown>, result:Record<string, unknown>}}
- */
-function UPDATE_QUERY(table:string, data:Map<string, string>, id:string|number):{query:string|undefined, data:Array<unknown>, result:Record<string, unknown>} {
-    let name:string|undefined;
-    switch (table){
-        case "jobs":
-            const employer = data.get("employer");
-            const startDate = data.get("startDate");
-            const endDate = data.get("endDate");
-            const title = data.get("title");
-            const about = data.get("about");
-
-            const job = {employer, startDate, endDate, title, about, id}
-            validateJobItem(job, false);
-            return {
-                query: typeof id === "string"
-                    ? "UPDATE Jobs SET employer = ? startDate = ? endDate = ? title = ? about = ? WHERE id = ?"
-                    : "INSERT INTO Jobs(employer, startDate, endDate, title, about, id) VALUES(?, ?, ?, ?, ?, ?)",
-                data: [employer, startDate, endDate, title, about, id],
-                result: job
-            }
-
-        case "school":
-            name = data.get("name");
-            const graduated = data.get("graduated");
-            const degree = data.get("degree");
-            const other = data.get("other");
-
-            const school = {name, graduated, degree, other, id}
-            validateSchoolItem(school, false);
-            return {
-                query: typeof id === "string"
-                ? "UPDATE School SET name = ? graduated = ? degree = ? other = ? WHERE id = ?"
-                : "INSERT INTO School(name, graduated, degree, other, id) VALUES(?, ?, ?, ?, ?)",
-                data: [name, graduated, degree, other, id],
-                result: school
-            }
-
-        case "skills":
-            name = data.get("name");
-            const list = data.get("list");
-            const info = data.get("info");
-            
-            const skill = {name, list, info, id};
-            validateSkillItem(skill, false);
-            return {
-                query: typeof id === "string"
-                    ? "UPDATE Skills SET name = ? list = ? info = ? WHERE id = ?"
-                    : "INSERT INTO Jobs(name, list, info, id) VALUES(?, ?, ?, ?)",
-                data: [name, list, info, id],
-                result: skill
-            }
-    }
-
-    return {
-        query: undefined,
-        data: [],
-        result: {}
-    }
-}
-
-/** Query Wrapper
- * 
- * @param db 
- * @param query 
- * @returns 
- */
-async function queryWrapper(db:D1Database, query:string):Promise<Record<string, unknown>[]> {
-    const {results, error} = await db.prepare(query).all();
-
-    if(error)
-        throw error;
-
-    return results;
-}
+import { UPDATE_QUERY, INSERT_QUERY, TABLES, ORDER_BY, DELETE, queryWrapper } from "./database";
 
 const DESCRIPTION = "Alex's resume and other skills.";
 const title = (table:string, id?:string) => `Resume - ${table}${id? `(${id})`: ""}`;
@@ -146,7 +45,7 @@ Editor.get("/:table/New", async(ctx)=>{
 Editor.post("/:table/New", async(ctx)=>{
     const table = (ctx.params.get("table") as string).toLocaleLowerCase();
     const id = Date.now();
-    const {query, data} = UPDATE_QUERY(table, ctx.formData, id);
+    const {query, data} = INSERT_QUERY(table, ctx.formData, id);
 
     if(query === undefined)
         throw new HttpError(404, `Unable to find ${table}!`);

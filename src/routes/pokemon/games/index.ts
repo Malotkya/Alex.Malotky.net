3fc0e7cababb33483723295c424a161eaf7c27cd
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-10-23 11:37:36 -0500

    Created file to export all pokemon games.

---
+++ src/routes/pokemon/games/index.ts
@@ -0,0 +1,28 @@
+import { yellow } from "./yellow";
+import { crystal } from "./crystal";
+import { ruby } from "./ruby";
+import { platinum } from "./platinum";
+import { white } from "./white";
+import { x } from "./x"
+import { sun } from "./sun";
+import { eevee } from "./eevee";
+import { diamond } from "./brilliantDiamond";
+import { red } from "./fireRed";
+import { gold } from "./heartGold";
+import { sapphire } from "./alphaSapphire";
+import {black} from "./black2";
+import {shield} from "./shield";
+import {violet} from "./violet";
+
+export const DEFAULT_INIT = "violet";
+export default {
+    yellow, red, eevee,
+    crystal, gold,
+    ruby, sapphire,
+    platinum, diamond,
+    white, black,
+    x,
+    sun,
+    shield,
+    violet
+};

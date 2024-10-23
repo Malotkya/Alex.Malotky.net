59939f60990e731980be7772f500c646ff51ff3e
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-10-23 14:38:54 -0500

    Removed global and mutliline flags.

--- Engine/View/RenderEnvironment/index.ts
+++ Engine/View/RenderEnvironment/index.ts
@@ -187,7 +187,7 @@ export default class RenderEnvironment {
             return;
 
         for(let script of update) {
-            this.run(script.replace(/<\/?script.*?>/gmi, ""));
+            this.run(script.replace(/<\/?script.*?>/i, ""));
         }
     }
 

a9c5103df664993f402165ea8aff4924da56448b
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-10-23 09:18:53 -0500

    fixed issue with match being too greedy.

--- Engine/View/RenderEnvironment/index.ts
+++ Engine/View/RenderEnvironment/index.ts
@@ -174,7 +174,7 @@ export default class RenderEnvironment {
             return console.warn("Body didn't change!");
         }
         RenderEnvironment.render(this._main, update);
-        this.scripts(compressContent(update).match(/<script .*>.*<\/script>/gm));
+        this.scripts(compressContent(update).match(/<script .*?>.*<\/script>/gm));
         this._mainHash = hash;
     }
 
@@ -187,7 +187,7 @@ export default class RenderEnvironment {
             return;
 
         for(let script of update) {
-            this.run(script.replace(/<script .*>(.*)<\/script>/gm, "$1"));
+            this.run(script.replace(/<script .*?>(.*)<\/script>/gm, "$1"));
         }
     }
 

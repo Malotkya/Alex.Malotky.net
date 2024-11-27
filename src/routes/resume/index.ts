8b9c239a42b13719d8e577af15069cf44981f748
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-11-27 09:26:57 -0600

    Updated resume routers to match new Data Objects.

--- src/routes/resume/index.ts
+++ src/routes/resume/index.ts
@@ -2,232 +2,23 @@
  * 
  * @author Alex Malotky
  */
-import { Router, Middleware, Content, Context, HttpError } from "zim-engine";
-import ResumeView , { SingleView, ListView, SingleEditView, ListEditView, EditMainView, ROUTE } from "./view";
-import { validateSchoolItem } from "./view/school";
-import { validateJobItem } from "./view/job";
-import { validateSkillItem } from "./view/skill";
-import { UPDATE_QUERY, INSERT_QUERY, TABLES, ORDER_BY, DELETE, queryWrapper } from "./database";
+import { Router, HttpError } from "zim-engine";
+import ResumeRouter, {DESCRIPTION} from "./router";
+import { JobDetailed, EditJob, JobEditListView, JobListView } from "./view/job";
+import { JobItem } from "./data/job";
+import { SchoolDetailed, EditSchool, SchoolEditListView, SchoolListView } from "./view/school";
+import { SchoolItem } from "./data/school";
+import { SkillDetailed, EditSkill, SkillEditListView, SkillListView } from "./view/skill";
+import { SkillItem } from "./data/skill";
+import ResumeView , { EditMainView } from "./view";
 import styles from "./style.scss";
 
-const DESCRIPTION = "Alex's resume and other skills.";
-const title = (table:string, id?:string) => `Resume - ${table}${id? `(${id})`: ""}`;
-
-const Resume = new Router("/Resume");
 const Editor = new Router("/Edit");
-Resume.use(Editor);
 
-/** Validate Login
- * 
- */
-Editor.all("*", async(ctx)=>{
-    const user = await ctx.getAuth();
-    if(user === null) {
-        ctx.redirect("/Login");
-    }
-})
-
-/** New Record Handler
- * 
- */
-Editor.get("/:table/New", async(ctx)=>{
-    const table = (ctx.params.get("table") as string).toLocaleLowerCase();
-    return ctx.render({
-        head: {
-            styles,
-            title: title(table, "New"),
-            meta: {
-                description: DESCRIPTION
-            }
-        },
-        body: {
-            main: SingleEditView(table, null)
-        }
-    });
-});
-Editor.post("/:table/New", async(ctx)=>{
-    const table = (ctx.params.get("table") as string).toLocaleLowerCase();
-    const id = Date.now();
-    const {query, data} = INSERT_QUERY(table, ctx.formData, id);
-
-    if(query === undefined)
-        throw new HttpError(404, `Unable to find ${table}!`);
-    try {
-        await ctx.env.DB.prepare(query).bind(...data).run();
-
-        return ctx.redirect(ROUTE(table, true, id));
-    } catch (e){
-        console.error(e);
-        throw new HttpError(500, "There was a problem updating from the database!");
-    }
-});
-
-
-/** Query Table Handler
- * 
- */
-function QueryTable(view:(t:string, r:Record<string,unknown>[])=>Content):Middleware {
-    return async function handleTableQuery(ctx){
-        const table = (ctx.params.get("table") as string).toLocaleLowerCase();
-        let query = TABLES[table];
-    
-        if(query === undefined){
-            throw new HttpError(404, `Unable to find ${table}!`);
-        }
-    
-        query += ORDER_BY[table];
-    
-        try {
-            const {results, error} = await ctx.env.DB.prepare(query).all();
-    
-            if(error)
-                throw error;
-    
-            ctx.render({
-                head: {
-                    styles,
-                    title: title(table),
-                    meta: {
-                        description: DESCRIPTION
-                    },
-                },
-                body: {
-                    main: view(table, results)
-                }
-            })
-    
-        } catch (e){
-            console.error(e);
-            throw new HttpError(500, "There was a problem querying the database!");
-        }
-        
-    }
-}
-Resume.get("/:table", QueryTable(ListView));
-Editor.get("/:table", QueryTable(ListEditView));
-
-/** Query Record Handler
- * 
- */
-function QueryRecord(view:(t:string, r:Record<string,unknown>)=>Content):Middleware {
-    return async function handleTableRecord(ctx){
-        const table = (ctx.params.get("table") as string).toLocaleLowerCase();
-        const id = ctx.params.get("id") as string;
-        let query = TABLES[table];
-    
-        if(query === undefined){
-            throw new HttpError(404, `Unable to find ${table}!`);
-        }
-    
-        query += "WHERE id = ?";
-    
-        try {
-            const result = await ctx.env.DB.prepare(query).bind(id).first();
-    
-            if(result === null){
-                throw new HttpError(404, `Unable to find id '${id}' in '${table}'!`);
-            }
-    
-            return ctx.render({
-                head: {
-                    styles,
-                    title: title(table, result["title"] as string || result["name"] as string || id),
-                    meta: {
-                        description: DESCRIPTION
-                    }
-                },
-                body: {
-                    main: view(table, result)
-                }
-            });
-        } catch (e){
-            console.error(e);
-            throw new HttpError(500, "There was a problem querying the database!");
-        }
-    }
-}
-Resume.get("/:table/:id", QueryRecord(SingleView));
-Editor.get("/:table/:id", QueryRecord(SingleEditView));
-
-/** Delete Record Handler
- * 
- */
-Editor.delete("/:table/:id", async(ctx:Context)=>{
-    const table = (ctx.params.get("table") as string).toLocaleLowerCase();
-    const id = ctx.params.get("id") as string;
-    const query = DELETE[table];
-
-    if(query === undefined)
-        throw new HttpError(404, `Unable to find ${table}!`);
-
-    try {
-        await ctx.env.DB.prepare(query).bind(id).run();
-        ctx.redirect(ROUTE(table, true));
-    } catch (e){
-        console.error(e);
-        throw new HttpError(500, "There was a problem deleting from the database!");
-    }
-});
-
-/** Update Record Handler
- * 
- */
-Editor.post("/:table/:id", async(ctx:Context)=>{
-    const table = (ctx.params.get("table") as string).toLocaleLowerCase();
-    const id = ctx.params.get("id") as string;
-    const {query, data, result} = UPDATE_QUERY(table, ctx.formData, id);
-
-    if(query === undefined)
-        throw new HttpError(404, `Unable to find ${table}!`);
-    try {
-        await ctx.env.DB.prepare(query).bind(...data).run();
-
-        return ctx.render({
-            head: {
-                styles,
-                title: title(table, result["title"] as string || result["name"] as string || id),
-                meta: {
-                    description: DESCRIPTION
-                }
-            },
-            body: {
-                main: SingleEditView(table, result, "Update Successful!")
-            }
-        });
-    } catch (e){
-        console.error(e);
-        throw new HttpError(500, "There was a problem updating from the database!");
-    }
-});
-
-
-/** Default Resume Handler
- * 
- */
-Resume.get(async(ctx)=>{
-    try {
-        const school = await queryWrapper(ctx.env.DB, "SELECT * FROM School ORDER BY graduated DESC LIMIT 6");
-        const jobs = await queryWrapper(ctx.env.DB, "SELECT * FROM Jobs ORDER BY startDate DESC LIMIT 6");
-        const skills = await queryWrapper(ctx.env.DB, "SELECT * FROM Skills");
-        
-        ctx.render({
-            head: {
-                styles,
-                title: "Resume",
-                meta: {
-                    description: DESCRIPTION
-                }
-            },
-            body: {
-                main: ResumeView(jobs.map(j=>validateJobItem(j)), school.map(s=>validateSchoolItem(s)), skills.map(s=>validateSkillItem(s)))
-            }
-        })
-
-    } catch (e){
-        console.error(e);
-        throw new HttpError(500, "There was a problem getting the resume!");
-    }
-});
+//Editor Table Handlers
+Editor.use(new ResumeRouter("Job", EditJob, JobEditListView, true));
+Editor.use(new ResumeRouter("School", EditSchool, SchoolEditListView, true));
+Editor.use(new ResumeRouter("Skill", EditSkill, SkillEditListView, true));
 
 /** Default Editor Handler
  * 
@@ -246,5 +37,45 @@ Editor.get(async(ctx)=>{
         }
     })
 })
+
+const Resume = new Router("/Resume");
+Resume.use(Editor);
+
+Resume.use(new ResumeRouter("Job", JobDetailed, JobListView))
+Resume.use(new ResumeRouter("School", SchoolDetailed, SchoolListView));
+Resume.use(new ResumeRouter("Skill", SkillDetailed, SkillListView));
+
+/** Default Resume Handler
+ * 
+ */
+Resume.get(async(ctx)=>{
+
+    try {
+        //"SELECT * FROM Jobs ORDER BY startDate DESC LIMIT 6"
+        const jobs = await ctx.query().getAll(JobItem)
+        //"SELECT * FROM School ORDER BY graduated DESC LIMIT 6"
+        const school = await ctx.query().getAll(SchoolItem);
+        //"SELECT * FROM Skills"
+        const skills = await ctx.query().getAll(SkillItem);
+        
+        ctx.render({
+            head: {
+                styles,
+                title: "Resume",
+                meta: {
+                    description: DESCRIPTION
+                }
+            },
+            body: {
+                main: ResumeView(jobs, school, skills)
+            }
+        })
+
+    } catch (e){
+        console.error(e);
+        throw new HttpError(500, "There was a problem getting the resume!");
+    }
+});
+
 
 export default Resume;
---
+++ src/routes/resume/router.ts
@@ -0,0 +1,179 @@
+import { Router, Middleware, Content, Context, HttpError } from "zim-engine";
+import {TypeOf} from "zim-engine/Validation";
+import { SchoolItem } from "./data/school";
+import { JobItem } from "./data/job";
+import { SkillItem } from "./data/skill";
+import styles from "./style.scss";
+
+const DATA_OBJECTS = {
+    "Skill": SkillItem,
+    "School": SchoolItem,
+    "Job": JobItem
+} as const;
+
+type SingleView<T extends keyof typeof DATA_OBJECTS> = (value:TypeOf<typeof DATA_OBJECTS[T]>)=>Content;
+type SingleEditView<T extends keyof typeof DATA_OBJECTS> = (value:TypeOf<typeof DATA_OBJECTS[T]>|null, message?:string)=>Content;
+type GroupView<T extends keyof typeof DATA_OBJECTS>  = (value:TypeOf<typeof DATA_OBJECTS[T]>[])=>Content;
+
+export const DESCRIPTION = "Alex's resume and other skills.";
+export const title = (table:string, id?:string) => `Resume - ${table}${id? `(${id})`: ""}`;
+export const ROUTE = (table:string, edit:boolean = false, id?:unknown) => `/Resume${edit?"/Edit":""}/${table.charAt(0).toLocaleUpperCase()+table.substring(1)}${id?"/"+String(id):""}`;
+
+export default class ResumeRouter<T extends keyof typeof DATA_OBJECTS> extends Router {
+    constructor(name:T, recordView:SingleEditView<T>, tableView:GroupView<T>, edit:true)
+    constructor(name:T, recordView:SingleView<T>, tableView:GroupView<T>, edit:false)
+    constructor(name:T, recordView:SingleView<T>, tableView:GroupView<T>)
+    constructor(name:T, recordView:SingleEditView<T>, tableView:GroupView<T>, edit:boolean = false) {
+        super(name);
+
+       if(edit) {
+
+            /** Validate Login
+             * 
+             */
+            this.all("*", async(ctx)=>{
+                const user = await ctx.getAuth();
+                if(user === null) {
+                    ctx.redirect("/Login");
+                }
+            });
+
+            /** Delete Record Handler
+             * 
+             */
+            this.delete("/:id", async (ctx:Context)=>{
+                const id = ctx.params.get("id")!;
+
+                try {
+                    await ctx.query().delete(DATA_OBJECTS[name] as any, {id});
+                } catch (e){
+                    console.error(e);
+                    throw new HttpError(500, `There was a probem deleted from ${name}!`);
+                }
+                
+                ctx.redirect(ROUTE(name, edit))
+            });
+
+            /** Update Record Handler
+             * 
+             */
+            this.post("/:id", async(ctx:Context)=>{
+                const id = ctx.params.get("id")!;
+                const data = await ctx.formData(DATA_OBJECTS[name] as any);
+
+                try {
+                    ctx.query().update(DATA_OBJECTS[name] as any, data, {id});
+                } catch (e){
+                    console.error(e);
+                    throw new HttpError(500, `There was a probem updating to ${name}!`);
+                }
+                
+                ctx.render({
+                    head: {
+                        styles,
+                        title: title(name, data["title"] || data["name"] || id),
+                        meta: {
+                            description: DESCRIPTION
+                        }
+                    },
+                    body: {
+                        main: recordView(data as any)
+                    }
+                });
+            });
+
+            /** New Record Handler
+             * 
+             */
+            this.get("/New", async(ctx)=>{
+                ctx.render({
+                    head: {
+                        styles,
+                        title: title(name, "New"),
+                        meta: {
+                            description: DESCRIPTION
+                        }
+                    },
+                    body: {
+                        main: recordView(null)
+                    }
+                });
+            });
+
+            /** New Record Handler
+             * 
+             */
+            this.post("/New", async(ctx)=>{
+                const id = Date.now();
+                const data = await ctx.formData(DATA_OBJECTS[name] as any);
+                data["id"] = id;
+            
+                try {
+                    await ctx.query().insert(DATA_OBJECTS[name] as any, data);
+                } catch (e){
+                    console.error(e);
+                    throw new HttpError(500, `There was a probem inserting into ${name}!`);
+                }
+
+                ctx.redirect(ROUTE(name, true, id));
+            });
+       } //End If Edit
+
+        /** Query Record Handler
+         * 
+         */
+        this.get("/:id", async(ctx:Context)=>{
+            const id = ctx.params.get("id")!;
+            let result:any|null;
+            try {
+                result = await ctx.query().get(DATA_OBJECTS[name] as any, {id});
+            } catch (e){
+                console.error(e);
+                throw new HttpError(500, `There was a problem querying ${name}!`);
+            }
+
+            if(result === null){
+                throw new HttpError(404, `Unable to find id '${id}' in '${name}'!`);
+            }
+
+            ctx.render({
+                head: {
+                    styles,
+                    title: title(name, result["title"] || result["name"] || id),
+                    meta: {
+                        description: DESCRIPTION
+                    }
+                },
+                body: {
+                    main: recordView(result as any)
+                }
+            });
+        });
+
+        /** Default Query Handler
+         * 
+         */
+        this.get(async(ctx)=>{
+            let results:any[];
+            try {
+                results = await ctx.query().getAll(DATA_OBJECTS[name] as any);
+            } catch (e){
+                console.error(e);
+                throw new HttpError(500, `There was a problem querying ${name}!`);
+            }
+
+            ctx.render({
+                head: {
+                    styles,
+                    title: title(name),
+                    meta: {
+                        description: DESCRIPTION
+                    },
+                },
+                body: {
+                    main: tableView(results)
+                }
+            })
+        });
+    }
+}

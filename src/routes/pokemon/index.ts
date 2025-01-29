b2ea0ef39e3a30533e70e6f74947b3e32013c341
Malotkya <47186158+Malotkya@users.noreply.github.com>
2025-01-29 10:32:44 -0600

    Made some changes, and switched to router page not handling any real render elements.

--- src/routes/pokemon/index.ts
+++ src/routes/pokemon/index.ts
@@ -4,87 +4,58 @@
  */
 import {Context, Router} from "zim-engine";
 import Game, { GameObject } from "./data/game";
-import PokemonView from "./view";
-import { EditPokemonPreview } from "./view/PokemonGame";
-import { recordHas, simplify } from "@/util";
-import styles from "./style.scss";
-import test from "./test.json";
+import PokemonView, { buildBaseData } from "./view";
+import { EditPokemonPreview } from "./view/PokemonEdit";
+import { recordHas } from "@/util";
+import Editor from "./edit";
 
-function buildData(list:Game[]):[Record<string, Game>, string] {
-    if(list.length === 0)
-        throw new Error("No Results!");
-
-    const output:Record<string, Game> = {};
-
-    /** Get Navigation Id
-     * 
-     * @param {Game} value 
-     * @returns {string}
-     */
-    const getId = (value:Game):string => {
-        const base = simplify(value.name);
-        let id = base;
-        let count = 0;
-        while(recordHas(output, id))
-            id = `${base}${++count}`;
-
-        return id;
+let data:Record<string, Game>|undefined, init:string|undefined;
+async function getData(ctx:Context):Promise<[Record<string, Game>, string]>{
+    if(data === undefined || init === undefined){
+        const [a, b] = buildBaseData(await ctx.query(GameObject).getAll());
+        data = a;
+        init = b;
     }
 
-    let init:string = getId(list[0]);
-    let max:number  = list[0].id || 0;
-    
-    output[init] = list[0];
-
-    for(let i=1; i<list.length; i++) {
-        const id = getId(list[i]);
-
-        if( max < (list[i].id || 0) ) {
-            init = id;
-            max = list[i].id!;
-        }
-
-        output[id] = list[i];
-    }
-
-    return [output, init];
+    return [data, init];
 }
 
 /** Pokemon Router
  * 
  */
 export const Pokemon = new Router("/Pokemon");
+Pokemon.use(Editor);
 
+/** Public Preview of Pokemon Editor
+ * 
+ */
 Pokemon.all("/New", async(ctx:Context)=>{
-    ctx.render({
-        head: {
-            styles,
-            title: "Edit Pokemon Game (Preview)",
-            meta: {
-                description: "Try editing your own pokemon save information!"
-            }
-        },
-        body: {
-            main: EditPokemonPreview()
-        }
-    });
-})
+    ctx.render(EditPokemonPreview());
+});
 
+/** Clear Cached Data
+ * 
+ */
+Pokemon.all("/Clear", async(ctx:Context)=>{
+    if(await ctx.getAuth()) {
+        data = undefined;
+        init = undefined;
+        ctx.redirect("/Pokemon");
+    }
+});
+
+/** View All Games
+ * 
+ */
 Pokemon.all(async(ctx:Context)=>{
-    const [data, init] = buildData([ GameObject.run(test) ])
+    const [data, defaultInit] = await getData(ctx);
+    const selectedInit = ctx.search.get("game") || "";
 
-    ctx.render({
-        head: {
-            styles,
-            title: "Pokemon Games",
-            meta: {
-                description: "Pokemon teams accross the different pokemon games Alex has played."
-            }
-        },
-        body: {
-            main: PokemonView(data, ctx.search.get("game") || init)
-        }
-    });
+    const init = recordHas(data, selectedInit)
+        ? selectedInit
+        : defaultInit;
+
+    ctx.render(PokemonView(data, init));
 });
 
 export default Pokemon;

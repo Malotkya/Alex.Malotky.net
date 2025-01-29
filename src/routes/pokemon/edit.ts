2c5b80381f891bd3931c82ab2ffdbc4d691c9e7b
Malotkya <47186158+Malotkya@users.noreply.github.com>
2025-01-29 10:32:14 -0600

    Added Seperate Edit Router file.

---
+++ src/routes/pokemon/edit.ts
@@ -0,0 +1,56 @@
+import {HttpError, Router} from "zim-engine";
+import { RequireLoginHandler } from "../login";
+import { GameObject } from "./data/game";
+import EditPokemonList, { EditPokemonGame } from "./view/PokemonEdit";
+
+const PokemonEdit = new Router("/Edit");
+
+PokemonEdit.all("*", RequireLoginHandler);
+
+PokemonEdit.delete("/:id", async(ctx)=>{
+    const id = Number(ctx.params.get("id"));
+    await ctx.query(GameObject).delete({id});
+    ctx.redirect("/Pokemon/Edit");
+});
+
+PokemonEdit.post("/New", async(ctx)=>{
+    const id = Date.now();
+    const game = await ctx.formData(GameObject);
+    game.id = id;
+
+    await ctx.query(GameObject).insert(game);
+
+    ctx.redirect(`/Decks/Edit/${id}`);
+});
+
+PokemonEdit.get("/New", async(ctx)=>{
+    ctx.render(EditPokemonGame(null));
+});
+
+PokemonEdit.post("/:id", async(ctx)=>{
+    const id = Number(ctx.params.get("id"));
+    const game = await ctx.formData(GameObject);
+
+    await ctx.query(GameObject).update(game, {id});
+
+    ctx.render(EditPokemonGame(game, id));
+});
+
+PokemonEdit.get("/:id", async(ctx)=>{
+    const id = Number(ctx.params.get("id"));
+    const game = await ctx.query(GameObject).get({id});
+
+    if(game === null)
+        throw new HttpError(404, `Unable to find deck with id '${id}'!`);
+
+    ctx.render(EditPokemonGame(game, id));
+});
+
+
+PokemonEdit.all(async(ctx)=>{
+    const results = await ctx.query(GameObject).getAll();
+    ctx.render(EditPokemonList(results));
+});
+
+
+export default PokemonEdit;

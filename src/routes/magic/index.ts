80b54cc3dbcc8521eb18c9788e297f2982f263b9
Malotkya <47186158+Malotkya@users.noreply.github.com>
2025-01-31 09:08:58 -0600

    Added preview page edit page to Mtg Decks.

--- src/routes/magic/index.ts
+++ src/routes/magic/index.ts
@@ -2,37 +2,18 @@
  * 
  * @author Alex Malotky
  */
-import {Router, HttpError} from "zim-engine";
-import { RequireLoginHandler } from "../login";
+import { Router, HttpError } from "zim-engine";
+import MagicEditor from "./edit";
 import { DeckItemObject } from "./data/deck";
-import { DeckEdit, DeckView, DeckListView } from "./view";
+import { DeckView, DeckListView, EditPreview } from "./view";
 import styles from "./style.scss";
 
 const PAGE_SIZE = 30;
 
 const Magic = new Router("/Decks");
-const Editor = new Router("/Edit");
+Magic.use(MagicEditor);
 
-Editor.all("*", RequireLoginHandler);
-
-Editor.delete("/:id", async(ctx)=>{
-    const id = Number(ctx.params.get("id")!);
-    await ctx.query(DeckItemObject).delete({id});
-    ctx.redirect("/Decks/Edit");
-});
-
-Editor.post("/New", async(ctx)=>{
-    const id = Date.now();
-    
-    const deck = await ctx.formData(DeckItemObject);
-    deck.id = id;
-
-    await ctx.query(DeckItemObject).insert(deck);
-
-    ctx.redirect(`/Decks/Edit/${id}`);
-});
-
-Editor.get("/New", async(ctx)=>{
+Magic.get("/New", async(ctx)=>{
     ctx.render({
         head: {
             styles,
@@ -42,84 +23,10 @@ Editor.get("/New", async(ctx)=>{
             }
         },
         body: {
-            main: DeckEdit(),
+            main: EditPreview(),
         }
     });
 });
-
-Editor.post("/:id", async(ctx)=>{
-    const id = Number(ctx.params.get("id")!);
-    const deck = await ctx.formData(DeckItemObject);
-    
-    await ctx.query(DeckItemObject).update(deck, {id});
-
-    ctx.render({
-        head: {
-            styles,
-            title: `Edit Deck (${id})`,
-            meta: {
-                description: "Magic the Gathering Deck Editor."
-            }
-        },
-        body: {
-            main: DeckEdit({...deck, id})
-        }
-    });
-});
-
-Editor.get("/:id", async(ctx)=>{
-    const id = Number(ctx.params.get("id")!);
-
-    const deck = await ctx.query(DeckItemObject).get({id});
-
-    if(deck === null)
-        throw new HttpError(404, `Unable to find deck with id '${id}'!`);
-
-    ctx.render({
-        head: {
-            styles,
-            title: `Edit Deck (${id})`,
-            meta: {
-                description: "Magic the Gathering Deck Editor."
-            }
-        },
-        body: {
-            main: DeckEdit(deck),
-        }
-    });
-});
-
-Editor.all(async(ctx)=>{
-    let page = Number(ctx.search.get("page"));
-    if(isNaN(page) || page < 0)
-        page = 0;
-    else
-        --page;
-
-    //SELECT * FROM Decks ORDER BY id DESC LIMIT ? OFFSET ?
-    const results = await ctx.query(DeckItemObject).getAll(); /*undefined,
-        {
-            orderBy: {id: "DESC"},
-            limit: PAGE_SIZE,
-            offset: PAGE_SIZE * page
-        }
-    ); */
-
-    ctx.render({
-        head: {
-            styles,
-            title: "List of Decks",
-            meta: {
-                description: "List of Magic the Gathering decks."
-            }
-        },
-        body: {
-            main: DeckListView(results, true)
-        }
-    });
-});
-
-Magic.use(Editor);
 
 Magic.get("/:id", async(ctx)=>{
     const id = Number(ctx.params.get("id")!);
--- src/routes/magic/view/index.ts
+++ src/routes/magic/view/index.ts
@@ -5,8 +5,8 @@
 import {Buffer} from "node:buffer";
 import {createElement as _, Content} from "zim-engine";
 import { DeckItem } from "../data/deck";
-
 import DeckView from "./deck";
+import DeckInput from "../element/DeckInput";
 export {DeckView};
 
 /** Deck Edit Module
@@ -29,12 +29,28 @@ export function DeckEdit(deck?:DeckItem): Content{
     );
 }
 
-function confirmDelete(event:Event){
-    if(!confirm("Are you sure?")) {
-        event.preventDefault();
-        event.stopPropagation();
-    }
+export function EditPreview():Content {
+    return [
+        _("h1", "Magic Deck Editor"),
+        _("aside", {class: "info"},
+            _("h2", "About"), 
+            _("p",
+                "This is the editor that I created so that I could easily store and display Magic the Gathering Decks on my website. ",
+                "While the form on this page doesn't have the ability to upload decks, it does have the ability to store them into a .json file to be downloaded. ",
+                "Because this is a custom form instead of a custom element wrapped around a form, this will not laod in safari.  This is a known error that I have no intention to fix because I am the only who really uses this, and refactoring it isn't high on my priority list."
+            ),
+            _("cache-downloader"),
+            _("p", {class: "error"}, "This form does not work in safari!"),
+        ),
+        _("section",
+            _("h2", "Load Data from File"),
+            _("input", {type: "file", onchange: loadFile})
+        ),
+        _("form", {id: "deck-input", is: "deck-input", onsubmit:saveFile})
+    ]
 }
+
+
 
 /** List View
  * 
@@ -78,4 +94,93 @@ export function DeckListView(list:Array<DeckItem>, edit:boolean = false):Content
             ))
         )
     ]
+}
+
+/** Front End Confirm Delete
+ * 
+ * @param {Event} event 
+ */
+function confirmDelete(event:Event){
+    if(!confirm("Are you sure?")) {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+}
+
+
+/** Load File FrontEnd Function
+ * 
+ * @param {Event} event 
+ */
+function loadFile(event:Event) {
+    (async()=>{
+        const files = (<HTMLInputElement>event.target).files
+        if(files){
+            const form = document.querySelector("form[is='deck-input']");
+            if(form === null) {
+                //@ts-ignore
+                env.error("Unable to find form!");
+                return;
+            }
+
+            const data = await files[0].text();
+            console.log(data);
+            form.setAttribute("data", btoa(unescape(encodeURIComponent(data))))
+        }
+    })();
+}
+
+/** Save File Frontend Function
+ * 
+ * @param {Event} event 
+ */
+function saveFile(event:Event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const form = <DeckInput>event.target;
+
+    const getValue = (query:string):string => {
+        const input = <HTMLInputElement|null>form.querySelector(query);
+        if(input === null) {
+            console.debug(form);
+            throw new Error(`${query} is null!`);
+        }
+            
+
+        return input.value;
+    }
+
+    const getData = ():Promise<DeckItem> => {
+        return new Promise((res, rej)=>{
+            try {
+                res({
+                    id:          undefined,
+                    name:        getValue("#name"),
+                    description: getValue("#description"),
+                    art:         getValue('[name="art"]'),
+                    color_identity: JSON.parse(getValue('[name="color_identity"]')),
+                    commanders:     JSON.parse(getValue('[name="commanders"]')),
+                    main_deck:      JSON.parse(getValue('[name="main_deck"]')), 
+                });
+            } catch (e){
+                rej(e);
+            }
+        })
+    }
+
+    getData().then(data=>{
+        const link = document.createElement("a");
+        link.href = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(data, null, 2));
+        link.download =`${data.name.replaceAll(/\s+/g, "_")}.json`;
+        link.style.display = "none";
+
+        document.body.appendChild(link);
+        link.click();
+
+        window.setTimeout(()=>{
+            document.body.removeChild(link);
+        }, 1);
+
+    }).catch(console.error);
 }
